7a91a2a5b1087008e5a117d309e49e937b829323
ameliapham <ptt.huong.8895@gmail.com>
2024-08-19 11:20:12 +0200

    Create TodoList and its basic style

---
+++ src/TodoList.tsx
@@ -0,0 +1,49 @@
+import { initialTodos } from "./data";
+import { tss } from "tss-react/mui"
+import { Typography } from "@mui/material";
+import { AddTodo } from "./components/AddTodo";
+
+
+type Props = {
+    className?: string
+}
+
+export function TodoList(props: Props) {
+
+    const { className } = props
+    const { classes } = useStyles()
+
+    return (
+        <div className={classes.root}>
+            <Typography
+                variant="h3"
+                className={classes.title}
+            >
+                Todo List 
+            </Typography>
+            <AddTodo
+                className={classes.addTodo}
+            />
+        </div>
+    )
+}
+
+const useStyles = tss.create({
+    "root": {
+        "height": "100vh",
+        "border": "1px solid red",
+        "display": "flex",
+        "flexDirection": "column",
+        "justifyContent": "center",
+        
+    },
+    "title": {
+        "display": "flex",
+        "justifyContent": "center",
+    },
+    "addTodo": {
+        "display": "flex",
+        "justifyContent": "center"
+    }
+
+})
