2fc9e5464c9d4d6f5ca3bbb1cdffb1130968a170
ameliapham <ptt.huong.8895@gmail.com>
2024-08-21 03:51:38 +0200

    Update UI of AddTodo part

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,30 +1,50 @@
-import Input from "@mui/material/Input"
+import TextField from '@mui/material/TextField'
 import Button from "@mui/material/Button"
 import { useState } from "react"
+import { tss } from "tss-react/mui"
 
 type Props = {
     className?: string,
-    onAddTodo: (params: {text: string}) => void
+    onAddTodo: (params: { text: string }) => void
 
 }
 
 export function AddTodo(props: Props) {
     const { className, onAddTodo } = props
+    const { classes, cx } = useStyles()
     const [inputValue, setInputValue] = useState("")
     return (
-        <div className={className}>
-            <Input
+        <div className={cx(className, classes.root)}>
+            <TextField
                 type="text"
                 value={inputValue}
                 onChange={e => setInputValue(e.target.value)}
+
+                className={classes.textField}
+                hiddenLabel
+                variant="standard"
+                size="small"
             />
             <Button
                 disabled={inputValue === ""}
-                onClick={()=> {
+                onClick={() => {
                     setInputValue("")
-                    onAddTodo({text: inputValue})
+                    onAddTodo({ text: inputValue })
                 }}
             >Add</Button>
         </div>
     )
 }
+
+const useStyles = tss.create({
+    "root": {
+        "width": "100%",
+        "border": "1px solid red",
+        "display": "flex",
+        "gap": "20px",
+        "margin": "20px 0 50px 0"
+    },
+    "textField": {
+        "flex" : "1"
+    }
+})
