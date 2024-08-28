5150a5e1ee165b58a3b0c7a8a479a901acb437c3
ameliapham <ptt.huong.8895@gmail.com>
2024-08-28 21:36:46 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { TodoList } from "./TodoList"
 import { GlobalStyles } from "@mui/material"
 
-
 export function App() {
   return (
     <div>
       <GlobalStyles styles={{
-          "*": {
+        "*": {
           "margin": 0,
           "padding": 0,
         },
-        }}/>
-        <TodoList />
+      }} />
+
+      <TodoList />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <App />
       </QueryClientProvider>
     </OidcProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
