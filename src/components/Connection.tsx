f7f4029e55de093d34d2a2c8b1f5a0c6abe0b51e
ameliapham <ptt.huong.8895@gmail.com>
2024-08-28 19:13:07 +0200

    Create Connection

---
+++ src/components/Connection.tsx
@@ -0,0 +1,35 @@
+import Button from "@mui/material/Button"
+import { tss } from "tss-react/mui"
+import Typography from "@mui/material/Typography"
+
+type Props = {
+    className?: string;
+}
+
+export function Connection(props: Props) {
+    const { className } = props
+    const { cx, classes } = useStyles()
+
+    return (
+        <div className={cx(className, classes.root)}>
+            <Typography
+                variant="h4"
+            >
+                Todo List
+            </Typography>
+
+            <Button>Connection</Button>
+        </div>
+    )
+}
+
+const useStyles = tss
+    .withName({ Connection })
+    .create({
+        "root": {
+            "display": "flex",
+            "justifyContent": "space-between",
+            "padding": "10px 30px",
+            "border": "1px solid red"
+        }
+    })
