9c620448afd9f698c83ca06f6fc359aa8469b519
ameliapham <ptt.huong.8895@gmail.com>
2024-08-28 17:23:39 +0200

    Apply useOidc

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -7,7 +7,7 @@ import { Todo } from "./components/Todo"
 import { useTodosApi } from "./todos-api"
 import { declareComponentKeys } from "i18nifty"
 import { useTranslation } from "./i18n/i18n"
-//import { useOidc } from "./oidc"
+import { useOidc } from "./oidc"
 import CircularProgress from '@mui/material/CircularProgress';
 
 
@@ -18,58 +18,61 @@ export function TodoList() {
 
     const { todos, createTodo, updateTodo, isPending, deleteTodo } = useTodosApi()
 
-    //const { oidcTokens } = useOidc();
+    const { oidcTokens } = useOidc();
 
     const { t } = useTranslation({ TodoList })
 
     if (todos === undefined) {
-        return <CircularProgress/>
+        return <CircularProgress />
     }
 
     return (
         <div className={classes.root}>
             {isPending && <CircularProgress />}
-            
+
             <Typography
                 variant="h4"
             >
-                {t("Todo List")}
+                {oidcTokens.decodedIdToken.name}{t("Todo List")}
             </Typography>
+
             <AddTodo
                 onAddTodo={({ text }) => createTodo(text)}
             />
-            <ul
-            //className={classes.listTodo}
-            >
+
+            <ul>
                 {todos.map(todo => (
                     <Todo
                         key={todo.id}
                         text={todo.text}
-                        done={todo.done}
+                        done={todo.isDone}
                         onDelete={() => deleteTodo(todo.id)}
                         onTextChange={({ text }) => updateTodo({ id: todo.id, text })}
                         onDoneChange={({ done }) => updateTodo({ id: todo.id, isDone: done })}
                     ></Todo>
                 ))}
             </ul>
+
         </div>
     )
 }
 
-const useStyles = tss.create({
-    "root": {
-        "backgroundColor": "#fad2e1",
-        "padding": "50px",
-        "borderRadius": "15px",
-        "width": "60vw",
-        "display": "flex",
-        "flexDirection": "column",
-        "position": "absolute",
-        "top": "10%",
-        "left": "50%",
-        "transform": "translate(-50%)",
-    },
-})
+const useStyles = tss
+    .withName({ TodoList })
+    .create({
+        "root": {
+            "backgroundColor": "#fad2e1",
+            "padding": "50px",
+            "borderRadius": "15px",
+            "width": "60vw",
+            "display": "flex",
+            "flexDirection": "column",
+            "position": "absolute",
+            "top": "10%",
+            "left": "50%",
+            "transform": "translate(-50%)",
+        },
+    })
 
 export const { i18n } = declareComponentKeys<
     | "Todo List"
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
-//import { OidcProvider } from "./oidc"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { OidcProvider } from "./oidc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <OidcProvider>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-  </React.StrictMode>
+    </OidcProvider>
+  </React.StrictMode>,
 )
