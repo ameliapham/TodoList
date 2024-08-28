30f7196a0a65d12daa356d734cfc2d5b454e3090
ameliapham <ptt.huong.8895@gmail.com>
2024-08-28 19:54:53 +0200

    Update translation

--- src/i18n/resources/en.tsx
+++ src/i18n/resources/en.tsx
@@ -2,6 +2,8 @@ import type { Translations } from '../types';
 
 export const translations: Translations<"en"> = {
     "Connection": {
-        "Todo List": "Todo List",
+        "Todo list": "Todo List",
+        "Log in": "Log in",
+        "Log out": "Log out"
     }
 };
--- src/i18n/resources/fr.tsx
+++ src/i18n/resources/fr.tsx
@@ -2,6 +2,8 @@ import type { Translations } from '../types';
 
 export const translations: Translations<"en"> = {
     "Connection": {
-        "Todo List": "Liste de tâches",
+        "Todo list": "Liste de tâches",
+        "Log in": "Se Connecter",
+        "Log out": "se Déconnecter"
     }
 };
