c12aa656fece05b763060287daedf7efbd31586e
ameliapham <ptt.huong.8895@gmail.com>
2024-08-30 21:23:55 +0200

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import { OidcProvider } from "./oidc";
@@ -7,11 +6,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <OidcProvider>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </OidcProvider>
-  </React.StrictMode>
 )
